1d0a90c42c2b0a9abd776d7e70c34e0616cd7142
DekangZeng <dzeng011@ucr.edu>
2022-01-10 22:13:46 +0800

    Initial Commit

---
+++ OpenGL_Template/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Template
+//
+//  Created by 曾德康 on 1/10/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
